5fa2f8cbde1c6861235d8eba45e410a795f44df7
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-08-24 00:43:16 -0400

    setup  webpack loaders and linter

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,13 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      title: 'Development',
+      title: 'Weather App',
+      template: './src/index.html'
     }),
   ],
   output: {
@@ -22,12 +25,22 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'css-loader',
           // Compiles Sass to CSS
-          "sass-loader",
+          'sass-loader',
         ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
     ],
   },
